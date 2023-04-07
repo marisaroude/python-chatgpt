af2ea3fb44979b4978efad8d759cef9fb15f54b9
marisaroude <81636065+marisaroude@users.noreply.github.com>
2023-04-07 19:56:27 -0300

    Modificación del archivo chatgpt incluyendo las estructuras Try/Except

--- chatgpt.py
+++ chatgpt.py
@@ -2,7 +2,7 @@
 import openai
 
 #……. [ mas Código de inicialización aqui ] …………
-openai.api_key = ("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")
+openai.api_key = ("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")
 TOP_P=1
 FREQ_PENALTY=0
 PRES_PENALTY=0
@@ -12,30 +12,29 @@ TEMPERATURE=0.75
 NMAX=1
 MODEL_ENGINE = "text-davinci-003"
 
-
 #…..[otra lógica necesaria – el texto del prompt debe colocarse en userText]…..
 # Set up the model and prompt
 
-while True: #bucle infinito que se ejecuta continuamente hasta que lo detiene explicitamente el break
+while True: #se ejecutará hasta el break. 
     userText = input("\nIngrese su consulta:" ) #En el userText se ingresa la consulta del usuario
-
-    completion = openai.Completion.create(
-        engine=MODEL_ENGINE,
-        prompt=userText,
-        max_tokens=MAX_TOKENS,
-        n=NMAX,
-        top_p=TOP_P,
-        frequency_penalty=FREQ_PENALTY,
-        presence_penalty=PRES_PENALTY,
-        temperature=TEMPERATURE,                
-        stop=STOP
-)
-    if len(userText) > 0:
-        print("You: ",userText)
-        print("ChatGPT: ",completion.choices[0].text)
-        break
-    else:
-        print("La consulta está vacía, por favor ingresa una consulta válida.")
-
-
-    
+    try:
+        if len(userText) > 0: #Verifica si la longitud del input es mayor a cero para aceptar una consulta valida y no vacía
+            completion = openai.Completion.create(  #puse el completion dentro del if porque al ingresar una consulta vacía me guardaba una respuesta random desde chatgpt y al hacer la consulta correcta me devolvía cualquier cosa, entonces, en este caso, primero se evalúa la longitud para invocar al completion
+                engine=MODEL_ENGINE,
+                prompt=userText,
+                max_tokens=MAX_TOKENS,
+                n=NMAX,
+                top_p=TOP_P,
+                frequency_penalty=FREQ_PENALTY,
+                presence_penalty=PRES_PENALTY,
+                temperature=TEMPERATURE,                
+                stop=STOP
+            )
+            print("You: ",userText) #devuelve la consulta 
+            print("ChatGPT: ",completion.choices[0].text) #devuelve la primer opción de respuesta desde la API
+            break
+        else: #Si la consulta esta vacia se produce una excepción en este caso value error que muestra que la consulta está vacía
+            raise ValueError("La consulta está vacía, por favor ingresa una consulta válida.") #este raise genera manualmente la excepcion
+    except ValueError as e:  #except maneja la excepción ya producida
+            print(e)
+   
