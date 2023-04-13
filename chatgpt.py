3061e52451aa353d850d09dcf113b946f035499f
marisaroude <81636065+marisaroude@users.noreply.github.com>
2023-04-13 19:04:39 -0300

    Mejora en el comment ratio, llegando a un valor de 35.47

--- chatgpt.py
+++ chatgpt.py
@@ -2,22 +2,20 @@
 import openai
 import sys
 #……. [ mas Código de inicialización aqui ] …………
-openai.api_key = ("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")
-USERNAME = 'You: '
-IA = 'chatGPT: '
-TOP_P=1
-FREQ_PENALTY=0
-PRES_PENALTY=0
-STOP=["You: ", "chatGPT: "]
-MAX_TOKENS=1024
-TEMPERATURE=0.75
-NMAX=1
-MODEL_ENGINE = "text-davinci-003"
- #Buffer para almacenar consultas no nulas.
+# Se establece la clave de la API de OpenAI y se definen algunas variables
+openai.api_key = ("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")
+USERNAME = 'You: ' # Define el prefijo del usuario
+IA = 'chatGPT: ' # Define el prefijo de la inteligencia artificial
+TOP_P = 1 # Define el parámetro top_p para el modelo de OpenAI
+FREQ_PENALTY = 0 # Define el parámetro frequency_penalty para el modelo de OpenAI
+PRES_PENALTY = 0 # Define el parámetro presence_penalty para el modelo de OpenAI
+STOP = ['You: ', 'chatGPT: '] # Define las palabras de parada para la conversación
+MAX_TOKENS = 1024 # Define la cantidad máxima de tokens para la generación de texto
+TEMPERATURE = 0.75 # Define el parámetro temperature para el modelo de OpenAI
+NMAX = 1 # Define el parámetro n para el modelo de OpenAI
+MODEL_ENGINE = "text-davinci-003" # Define el motor del modelo de OpenAI
 
-
-#…..[otra lógica necesaria – el texto del prompt debe colocarse en userText]…..
-# Set up the model and prompt
+#funcion para generar el completion y no repetir en la funcion de chatgpt_queris
 def generate_completion(MODEL_ENGINE,prompt, MAX_TOKENS, NMAX, TOP_P, FREQ_PENALTY, PRES_PENALTY, TEMPERATURE, STOP):
     completion = openai.Completion.create(
                 engine=MODEL_ENGINE,
@@ -33,18 +31,18 @@ def generate_completion(MODEL_ENGINE,prompt, MAX_TOKENS, NMAX, TOP_P, FREQ_PENAL
     return completion.choices[0].text
 
 
-
+#Funcion para activar la conversación con chatgpt, en principio comienza con la variable with_context en false
 def chatgpt_queris(with_context = False):
-    buffer = ''
+    buffer = '' '' #buffer para añadir las consultas.
     if with_context == False:
-        while True:
+        while True: #Mientras se siga ingresando consultas se ejecutará el ciclo hasta que escriban por consola "exit"
             try:
                 print("Ingrese una consulta válida (o escriba 'exit' para salir): ")
                 userText = input("You: ")
                 if len(userText) == 0:
                     raise ValueError("Error: La consulta está vacía") #este raise genera manualmente la excepcion
                 elif userText == 'exit':
-                    break
+                    break #Si el cliente ingresa por consola "exit" se finaliza el programa
             except ValueError as e:
                 print(e)
             else:
